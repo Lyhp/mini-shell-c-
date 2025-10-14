1a6e4960a36be9b2598922764cdba0422d1eb6ef
Lisbeth Huanca <lishuanca34@gmail.com>
2025-10-14 01:28:16 -0500

    CRea encabezado y estructura inicial

---
+++ include/builtins.hpp
@@ -0,0 +1,13 @@
+/**
+* @file builtins.hpp
+* @brief Comandos internos del shell (no usan fork)
+*/
+
+#ifndef BUILTINS_HPP
+#define BUILTINS_HPP
+
+#include "command.hpp"
+#include <string>
+#include <vector>
+
+#endif
