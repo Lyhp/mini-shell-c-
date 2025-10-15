3221a7902f369edb4d47e4ef4c9c35a12b834995
Lisbeth Huanca <lishuanca34@gmail.com>
2025-10-14 22:54:11 -0500

    Definir estructura base con clase shell()

---
+++ include/shell.hpp
@@ -0,0 +1,18 @@
+/**
+* @file shell.hpp
+* @brief Shell principal - orquesta todo el sistema
+*/
+
+#ifndef SHELL_HPP
+#define SHELL_HPP
+
+#include <string>
+
+class Shell {
+public:
+Shell();
+//Inicia el loop principal del shell
+void run();
+};
+
+#endif
