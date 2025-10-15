72b97efdbcf36a36611dd096852178cc6bd0df85
Lisbeth Huanca <lishuanca34@gmail.com>
2025-10-14 22:37:19 -0500

    Agregar estrucutura y dependencias

---
+++ include/parser.hpp
@@ -0,0 +1,13 @@
+ /**
+ @file parser.hpp
+* @brief Parser simplificado para convertir texto en comando
+*/
+
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include "command.hpp"
+#include <string>
+#include <vector>
+
+#endif
