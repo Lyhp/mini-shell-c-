3a2c2ed998f8e4e35864f9902f0c973cf782449a
Lisbeth Huanca <lishuanca34@gmail.com>
2025-10-14 21:42:24 -0500

    Agregar metodoos publico y privado principales

--- include/executor.hpp
+++ include/executor.hpp
@@ -5,4 +5,18 @@
 #include <vector>
 #include <sys/types.h>
 
+class Executor {
+public:
+//Ejecuta un pipeline compelto
+static int execute(const Pipeline& pipeline);
+
+private:
+
+static int execute_simple(const Command& cmd);
+static int execute_pipeline(const Pipeline& pipeline);
+static bool setup_redirections(const Command& cmd);
+static std::string resolve_path(const std::string& program);
+static void collect_zombies();
+};
+
 #endif
