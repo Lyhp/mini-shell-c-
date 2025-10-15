371d601a221a7d93e380a6f56ad3c0c466abe638
Lisbeth Huanca <lishuanca34@gmail.com>
2025-10-14 21:08:33 -0500

    Crear encabezado incial

---
+++ include/command.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file command.hpp
+ * @brief Define estructuras para representar comandos y pipelines.
+ */
+
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <string>
+#include <vector>
+
+#endif
