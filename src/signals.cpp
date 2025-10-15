490ddf3a5b41179105d310d5909bf29648d6f57d
feranahi2 <156111357+feranahi2@users.noreply.github.com>
2025-10-15 00:04:17 -0500

    Configuracion de manejadores de señales en setup_signals

--- src/signals.cpp
+++ src/signals.cpp
@@ -9,3 +9,15 @@ void sigint_handler(int sig) {
     std::cout << std::endl;
     write(STDOUT_FILENO, "minishell$ ", 11);
 }
+
+void setup_signals() {
+    //Configurar SIGINT (Ctrl+C)
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    
+    if (sigaction(SIGINT, &sa, nullptr) < 0) {
+        std::cerr << "Error configurando SIGINT" << std::endl;
+    }
+}
